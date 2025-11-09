3cac97737d8b95b714e532c645797c3152e44e23
Pedro Polastri Patriota <41030657+pedropatriota@users.noreply.github.com>
2025-11-09 20:10:38 +0000

    update favicon

---
+++ frontend/src/pages/TaxiOg.tsx
@@ -0,0 +1,5 @@
+import og from "../assets/og.png";
+
+const TaxiOg = () => <img src={og} alt="taxi og image" />;
+
+export default TaxiOg;
---
+++ frontend/src/pages/taxiOg.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import og from "../assets/og.png";
+
+const TaxiOg = () => <img src={og} alt="taxi og image" />;
+
+export default TaxiOg;
